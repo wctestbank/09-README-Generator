5605daa68a12019099450ab01a5b768a8bb649b4
wctestbank <wc.businessmail@gmail.com>
2022-05-16 10:30:19 -0400

    generate markdown planning

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,47 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  # ${data.title}
+  ## Description
+
+  ${data.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## License
+  
+  ${data.license}
+
+  ## Contributing
+  
+  ${data.contributing}
+
+  ## Tests
+  
+  ${data.tests}
+
+  ## Questions
+  
+  ${data.questions}
+  
+
 
 `;
 }
